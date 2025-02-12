c7c54c6cc373d9df2e4327b571a1d44fb3376a42
vinicius terra <viniterra33@gmail.com>
2025-02-12 17:23:13 -0300

    adiciona filtro de pesquisa

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -2,22 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { BsFillCartCheckFill, BsFillCartPlusFill } from 'react-icons/bs';
 import { getItem, setItem } from '../services/LocalStorageFuncs';
 import { Link } from 'react-router-dom';
-import { ProductArea  } from '../css/style';
+import { ProductArea } from '../css/style';
 import { Header } from '../components/Header';
 
 export const Store = () => {
     const [data, setData] = useState([]);
     const [cart, setCart] = useState(getItem('carrinhoyt') || []);
+    const [search, setSearch] = useState(''); 
+    const [categories, setCategories] = useState([]); 
+    const [selectedCategory, setSelectedCategory] = useState(''); 
 
     useEffect(() => {
-        const fetchApi = async () => {
-            const url = 'http://localhost:5000/products';
+        const fetchProducts = async () => {
+            const queryParams = new URLSearchParams();
+            if (search) queryParams.append('name', search);
+            if (selectedCategory) queryParams.append('categoryId', selectedCategory);
+
+            const url = `http://localhost:5000/products?${queryParams.toString()}`;
             const response = await fetch(url);
             const objJson = await response.json();
             setData(objJson);
-            console.log(objJson);
         };
-        fetchApi();
+        fetchProducts();
+    }, [search, selectedCategory]); 
+
+    
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const response = await fetch('http://localhost:5000/categories');
+            const objJson = await response.json();
+            setCategories(objJson);
+        };
+        fetchCategories();
     }, []);
 
     useEffect(() => {
@@ -38,7 +54,47 @@ export const Store = () => {
 
     return (
         <div>
-            <Header/>
+            <Header />
+
+            {/* Filtros */}
+            <div style={{ textAlign: 'center', margin: '20px 0' }}>
+                {/* Campo de pesquisa */}
+                <input
+                    type="text"
+                    placeholder="Buscar produto..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    style={{
+                        padding: '10px',
+                        width: '300px',
+                        fontSize: '16px',
+                        borderRadius: '5px',
+                        border: '1px solid #ccc',
+                        marginRight: '10px'
+                    }}
+                />
+
+                {/* Seletor de categorias */}
+                <select
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    style={{
+                        padding: '10px',
+                        fontSize: '16px',
+                        borderRadius: '5px',
+                        border: '1px solid #ccc'
+                    }}
+                >
+                    <option value="">Todas as categorias</option>
+                    {categories.map((cat) => (
+                        <option key={cat.id} value={cat.id}>
+                            {cat.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            {/* Lista de produtos */}
             <ProductArea>
                 {data.map((e) => (
                     <div key={e.id}>
@@ -47,9 +103,9 @@ export const Store = () => {
                         <h4>{`R$ ${e.price}`}</h4>
                         <button onClick={() => handleClick(e)}>
                             {cart.some((itemCart) => itemCart.id === e.id) ? (
-                                <BsFillCartCheckFill size={40}/>
+                                <BsFillCartCheckFill size={40} />
                             ) : (
-                                <BsFillCartPlusFill size={40}/>
+                                <BsFillCartPlusFill size={40} />
                             )}
                         </button>
                     </div>
